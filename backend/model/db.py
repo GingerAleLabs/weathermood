ef01827b08b661b4a458632da40a2db690b95a47
Alessandra <a.gattav@gmail.com>
2025-11-17 13:24:29 +0100

    test: MoodService get_entries

--- backend/model/db.py
+++ backend/model/db.py
@@ -4,46 +4,41 @@ import sqlite3
 DEFAULT_DB_PATH = Path(__file__).parent / "weathermood.db"
 SCHEMA_PATH = Path(__file__).parent / "schema.sql"
 
-DB_PATH = None
+DB_PATH: Path | None = None
 
-_initialized = False
 
 # if called before get_connection(), allows to change the path of the DB
 # Useful to run tests with a separate db file 
 def configure_db(path):
     global DB_PATH
-    DB_PATH = path
+    DB_PATH = Path(path)
 
-
+# Returns a connection to the db
+# If the db file does not exist yet,
+# this function creates and initializes it
 def get_connection():
 
     global DB_PATH
     if DB_PATH is None:
         DB_PATH = DEFAULT_DB_PATH
 
-    global _initialized
-    if not _initialized:
+    if not DB_PATH.exists():
         init_db()
-        _initialized = True
-    
+
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
 
-
+# Creates the db and initializes it using the SCHEMA_PATH script
 def init_db():
-    global _initialized
-    if _initialized:
-        return
-    
     global DB_PATH
-    db_file = Path(DB_PATH)
-    if not db_file.exists():
-        with sqlite3.connect(db_file) as conn:
-            with open(SCHEMA_PATH) as f:
-                conn.executescript(f.read())
+    if DB_PATH is None:
+        DB_PATH = DEFAULT_DB_PATH
 
-    _initialized = True
+    db_file = Path(DB_PATH)
+    with sqlite3.connect(db_file) as conn:
+        with open(SCHEMA_PATH, "r", encoding="utf-8") as f:
+            conn.executescript(f.read())
 
 def insert_mock_data():
     conn = get_connection()
--- tests/test_mood_service.py
+++ tests/test_mood_service.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import datetime
 from backend.model.db import configure_db, init_db, get_connection
 from backend.services.MoodEntryService import MoodEntryService
 
@@ -21,7 +22,7 @@ def test_add_entry(tmp_path):
     rows = cur.fetchall()
     conn.close()
 
-    # 5. Assertions
+    # Assertions
     assert len(rows) == 1
     row = rows[0]
 
@@ -31,3 +32,98 @@ def test_add_entry(tmp_path):
     assert row["weather_rating"] == 3
     assert row["user_id"] is None
     assert row["timestamp"] is not None
+
+
+def test_add_entry(tmp_path):
+    # Using a dedicated test db
+    test_db = tmp_path / "test.db"
+    configure_db(str(test_db)) 
+
+    MoodEntryService.add_entry(
+        user_id=None,
+        mood=4,
+        note="Feeling good today",
+        temperature=22.5,
+        weather_rating=3,
+    )
+
+    # Check the entry was correctly inserted
+    conn = get_connection()
+    cur = conn.execute("SELECT * FROM mood_entry")
+    rows = cur.fetchall()
+    conn.close()
+
+    # Assertions
+    assert len(rows) == 1
+    row = rows[0]
+
+    assert row["mood"] == 4
+    assert row["note"] == "Feeling good today"
+    assert abs(row["temperature"] - 22.5) < 0.001
+    assert row["weather_rating"] == 3
+    assert row["user_id"] is None
+    assert row["timestamp"] is not None
+
+
+
+def test_get_entries(tmp_path):
+    # Using a dedicated test db
+    test_db = tmp_path / "test.db"
+    configure_db(str(test_db)) 
+
+    # Insert 3 entries
+    conn = get_connection()
+    cur = conn.cursor()
+    timestamp1 = datetime.now().isoformat()
+    cur.execute(
+        """
+        INSERT INTO mood_entry (user_id, timestamp, mood, note, temperature, weather_rating)
+        VALUES (?, ?, ?, ?, ?, ?)
+        """,
+        (None, timestamp1, 3, "Just okay", 16.1, 2)
+    )
+    timestamp2 = datetime.now().isoformat()
+    cur.execute(
+        """
+        INSERT INTO mood_entry (user_id, timestamp, mood, note, temperature, weather_rating)
+        VALUES (?, ?, ?, ?, ?, ?)
+        """,
+        (None, timestamp2, 1, "Less than ideal", 5.7, 0)
+    )
+    timestamp3 = datetime.now().isoformat()
+    cur.execute(
+        """
+        INSERT INTO mood_entry (user_id, timestamp, mood, note, temperature, weather_rating)
+        VALUES (?, ?, ?, ?, ?, ?)
+        """,
+        (None, timestamp3, 5, "Feelin' great :)", 22.3, 4)
+    )
+    conn.commit()
+    conn.close()
+
+    entries = MoodEntryService.get_entries()
+
+    # Assertions
+    assert len(entries) == 3
+    
+    #The entries will be returned in reverse chronological order, by design
+    entry1 = entries[2]
+    assert entry1["mood"] == 3
+    assert entry1["note"] == "Just okay"
+    assert abs(entry1["temperature"] - 16.1) < 0.001
+    assert entry1["weather_rating"] == 2
+    assert entry1["timestamp"] == timestamp1
+
+    entry1 = entries[1]
+    assert entry1["mood"] == 1
+    assert entry1["note"] == "Less than ideal"
+    assert abs(entry1["temperature"] - 5.7) < 0.001
+    assert entry1["weather_rating"] == 0
+    assert entry1["timestamp"] == timestamp2
+
+    entry1 = entries[0]
+    assert entry1["mood"] == 5
+    assert entry1["note"] == "Feelin' great :)"
+    assert abs(entry1["temperature"] - 22.3) < 0.001
+    assert entry1["weather_rating"] == 4
+    assert entry1["timestamp"] == timestamp3
